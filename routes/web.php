a4595789bb7a89c8603ea4e86c5e53e671c9d229
Muhammad Mirza <mirzaalam557@gmail.com>
2025-01-15 21:39:25 +0700

    guest

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,10 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'authenticating']);
-Route::get('register', [AuthController::class, 'register']);
+Route::get('login', [AuthController::class, 'login'])->name('login')->middleware('guest');
+Route::post('login', [AuthController::class, 'authenticating'])->middleware('guest');
+Route::get('register', [AuthController::class, 'register'])->middleware('guest');
 
 Route::get('dashboard', [DashboardController::class, 'index'])->middleware('auth', 'only_admin');
 Route::get('profile', [UserController::class, 'profile'])->middleware('auth', 'only_client');
-Route::get('books', [BookController::class, 'index']);
+Route::get('books', [BookController::class, 'index'])->middleware('auth');
